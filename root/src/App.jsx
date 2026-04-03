645eed486d0dec64fbb9746e8026600f9586259d
aviraL27 <aviral.5015.xo@gmail.com>
2026-04-04 02:55:14 +0530

    added final files for frontend

---
+++ root/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import AppRoutes from './routes/index';
+import { ThemeProvider } from './context/ThemeContext';
+import { useAuthStore } from './store/authStore';
+
+function SessionBootstrap() {
+  const initializeAuth = useAuthStore((state) => state.initializeAuth);
+
+  useEffect(() => {
+    initializeAuth();
+  }, [initializeAuth]);
+
+  return null;
+}
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <SessionBootstrap />
+      <AppRoutes />
+    </ThemeProvider>
+  );
+}
